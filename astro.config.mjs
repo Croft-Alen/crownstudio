3c0313ae8bf7c279aee37406d86b4dd69c3a9947
haseebdigit <haseebdigit@gmail.com>
2026-07-30 13:18:03 +0500

    Downgrade Astro to 4.x for Vercel compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://crownstudio.com",
   output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
   vite: {
     server: {
       fs: {
