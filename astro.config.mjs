ad26fa0dca1c3c8a82d40f63719ac9182d430697
haseebdigit <haseebdigit@gmail.com>
2026-07-30 15:15:09 +0500

    Switch to static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://crownstudio-1337.vercel.app",
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  build: {
-    client: ".vercel/output/static",
-    server: ".vercel/output/functions/server"
+  output: "static",
+  vite: {
+    server: {
+      fs: {
+        allow: ['..']
+      }
+    }
   }
 });
