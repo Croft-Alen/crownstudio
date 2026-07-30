4f483ca52bd22c854f73bf6e7171f0a998be30b1
haseebdigit <haseebdigit@gmail.com>
2026-07-30 13:59:13 +0500

    Fix Vercel routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://crownstudio.com",
+  site: "https://crownstudio-1337.vercel.app",
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
+  build: {
+    client: "dist/client",
+    server: "dist/server"
+  },
   vite: {
     server: {
       fs: {
