e3bb2c0c35b828ab543affe9e37652070cb1fbfa
haseebdigit <haseebdigit@gmail.com>
2026-07-30 14:46:42 +0500

    Use entry.mjs directly

--- server.js
+++
@@ -1,8 +0,0 @@
-import { App } from './dist/server/entry.mjs';
-
-const app = new App();
-const port = process.env.PORT || 4321;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
